bcd3a37a713989c71929d760a92bc2fb14435451
felabel <felicityabel2016@gmail.com>
2025-02-10 08:58:26 +0100

    updated base url

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  // uri: "http://localhost:4000",
+  uri: "https://catstronaut-n6mh.onrender.com/",
   cache: new InMemoryCache(),
 });
 
